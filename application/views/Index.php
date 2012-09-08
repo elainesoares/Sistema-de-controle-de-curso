e999f2835a892a21825a187cdbb87223002924cf
Felipe Maia <felipepqm@gmail.com>
2012-09-08 15:43:33 -0300

    Configurando Views. Html + css

---
+++ application/views/Index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Centro de cursos para tratamento do Câncer</title>
+<link rel="stylesheet" href="assets/css/reset.css" type="text/css" />
+
+<link rel="stylesheet" type="text/css" href="assets/css/template.css" />
+</head>
+
+<body>
+<div id="box">
+	<div id="container">
+	<div id="header">
+    	
+    	<div id="header_center">
+    		<div id="logo"> 
+            	<img src="assets/Images/LOGOMARCA-FUNDAÇÃO-SARA-ORIGINAL2.fw.png" />       
+       		</div>
+            <div id="descricao">
+            	<img src="assets/Images/frasesara.png" />
+            </div>
+            <div id="menu_header">
+            	<ul>
+                	<li><a href=""> Home</a> </li>
+                    
+                    <li><a href=""> Cadastrar</a> </li>
+                  	
+                    <li><a href=""> Pesquisar </a></li>
+                   
+                    <li><a href=""> Relatórios </a></li>
+                    
+                    <li><a href=""> Ajuda </a></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="under_header">
+ 		<div id="nuvem1"></div>
+        <div id="sol"> </div>
+        <div id="nuvem2"></div>       	
+    </div>
+    <div class="clear"> </div>
+    <div id="conteudo">
+    	<div id="titulo">
+    		<h2>Home</h2>
+   		</div>
+        <div id="under_title"> 
+        </div>
+        <div id="caixa">
+        	<div id="caixa_top">
+        	</div>
+        	<div id="caixa_center">
+        	<p> A Fundação Sara em parceria com o instituto Ronald McDonald promove a disseminação dos conhecimentos relativos ao câncer. Esse Sistema foi criado pela INFOBITS - associação civil sem fins lucrativos residida na UNIMONTES e administrada por alunos e professores - e vem contribuir com uma eficácia de gerenciamento dos cursos. Caso necessite de ajuda,<a href=> clique aqui</a> e veja alguns tutoriais.
+            </p>	
+        	</div>
+        	<div id="caixa_bottom">
+        	</div>
+        </div>    
+        
+    </div>
+    <div id="imagem1">    		    		           
+   	</div>
+    <div class="clear"> </div>
+    
+     
+    
+    
+</div>
+
+</div>
+<div id="grama">
+    </div>
+<div id="rodape">
+	<div id="rodape_conteudo">
+    </div>	
+</div>
+</body>
+</html>
